f5575865d649b7cf65c61227ea7b58674ecd244a
Daniel MacMillan <dmacmillan@dmacmillan01.phage.bcgsc.ca>
2016-05-27 09:32:21 -0700

    Fixed parsing of GTF files and searching for minimal distances

--- novelCleavage.py
+++ novelCleavage.py
@@ -1,4 +1,5 @@
 import argparse, os, sys
+from bisect import bisect_left
 from Kleat import *
 from GTF import *
 
@@ -16,131 +17,55 @@ def parseSummary(summary):
             results[line[11]] = [line[0], line[2], line[7]]
     return results
 
-def parseSimpleKleat(kleat,
-                     max_dist_ann = float('inf'), 
-                     min_len_tail_contig = None,
-                     min_num_tail_reads = None,
-                     min_num_bridge_reads = None,
-                     min_bridge_read_tail_len = None,
-                     has_pas = None):
-    results = []
-    with open(kleat, 'r') as f:
-        header = f.readline()
-        for line in f:
-            result = Kleat(*line.strip().split('\t'))
-            if (result.length_of_tail_in_contig < min_len_tail_contig):
-                if (result.number_of_bridge_reads < min_num_bridge_reads):
-                    continue
-            elif min_num_tail_reads and (0 <= result.number_of_tail_reads < min_num_tail_reads):
-                continue
-            elif min_bridge_read_tail_len and result.number_of_bridge_reads > 0 and (0 <= result.max_bridge_read_tail_length < min_bridge_read_tail_len):
-                continue
-            elif (result.distance_from_annotated_site > max_dist_ann):
-                continue
-            elif has_pas and not result.pas:
-                continue
-            result.ID = os.path.basename(kleat).split('.')[0]
-            results.append(result)
-    return results
+def minDistanceBinarySearch(a, x, lo=0, hi=None):   # can't use a to specify default for hi
+    hi = hi if hi is not None else len(a) # hi defaults to len(a)   
+    pos = bisect_left(a,x,lo,hi)          # find insertion position
+    #return (pos if pos != hi and a[pos] == x else -1) # don't walk off the end
+    if pos != hi and a[pos] == x:
+        return pos
+    elif pos != 0:
+        return pos-1
+    else:
+        return 0
 
-def parseKleat(kleat, 
-               max_dist_ann = float('inf'), 
-               min_len_tail_contig = None,
-               min_num_tail_reads = None,
-               min_num_bridge_reads = None,
-               min_bridge_read_tail_len = None,
-               has_pas = None):
-    results = []
-    with open(kleat, 'r') as f:
-        header = f.readline()
-        for line in f:
-            result = Kleat(*line.strip().split('\t'))
-            if min_len_tail_contig and (0 <= result.length_of_tail_in_contig < min_len_tail_contig):
-                continue
-            elif min_num_tail_reads and (0 <= result.number_of_tail_reads < min_num_tail_reads):
-                continue
-            elif min_num_bridge_reads and (0 <= result.number_of_bridge_reads < min_num_bridge_reads):
-                continue
-            elif min_bridge_read_tail_len and (0 <= result.max_bridge_read_tail_length < min_bridge_read_tail_len):
-                continue
-            elif (result.distance_from_annotated_site > max_dist_ann):
-                continue
-            elif has_pas and not result.pas:
-                continue
-            result.ID = os.path.basename(kleat).split('.')[0]
-            results.append(result)
-    return results
+def groupBy(group, att):
+	for c in group:
+		for g in group[c]:
+			strand = group[c][g][0].strand
+			transcripts = {}
+			for x in group[c][g]:
+				tid = x.attribute[att]
+				if tid not in transcripts:
+					transcripts[tid] = [x]
+				else:
+					transcripts[tid].append(x)
+			group[c][g] = transcripts
 
-def groupKleat(parsed):
-    results = {}
-    for r in parsed:
-        if r.chromosome not in results:
-            results[r.chromosome] = {r.gene: [r]}
-        if r.gene not in results[r.chromosome]:
-            results[r.chromosome][r.gene] = [r]
-        else:
-            results[r.chromosome][r.gene].append(r)
-    return results
-
-def findNovelCleavageEvents(kleats_grouped, gtf_grouped, distance=20):
+def findNovelCleavageEvents(kleats_grouped, pos, neg, distance=20):
     results = []
     kg = kleats_grouped
-    gg = gtf_grouped
     for chrom in kg:
-        if chrom not in gg:
-            continue
         for gene in kg[chrom]:
-            if gene not in gg[chrom]:
-                continue
-            strand = gg[chrom][gene][0].strand
             sites = [x.cleavage_site for x in kg[chrom][gene]]
-            utrs = gg[chrom][gene]
+            strand = kg[chrom][gene][0].transcript_strand
             for i,site in enumerate(sites):
-                my_min = float('inf')
-                closest_utr = None
-                for utr in utrs:
-                    current_min = min([abs(site - utr.start), abs(utr.end - site)])
-                    if current_min <= my_min:
-                        my_min = current_min
-                        closest_utr = utr
+                if strand == '+':
+                    my_min = abs(site - pos[minDistanceBinarySearch([x.end for x in pos], site)].end)
+                    closest_utr = pos[i]
+                else:
+                    my_min = abs(site - neg[minDistanceBinarySearch([x.start for x in neg], site)].start)
+                    closest_utr = neg[i]
                 if my_min > distance:
                     results.append([kg[chrom][gene][i], closest_utr, my_min])
     return results
-
-def groupEnsemblGTF(gtf):
-    result = {}
-    for g in gtf:
-        if g.seqname not in result:
-            result[g.seqname] = {g.attribute['gene_name']: [g]}
-        if g.attribute['gene_name'] not in result[g.seqname]:
-            result[g.seqname][g.attribute['gene_name']] = [g]
-        else:
-            result[g.seqname][g.attribute['gene_name']].append(g)
-    #for chrom in result:
-    #    for gene in result[chrom]:
-    #        result[chrom][gene].sort(key=lambda x: x.start)
-    return result
-
-def groupAceviewGTF(gtf, result=None):
-    if not result:
-        result = {}
-    for g in gtf:
-        if g.seqname not in result:
-            result[g.seqname] = {g.attribute['gene_id']: [g]}
-        if g.attribute['gene_id'] not in result[g.seqname]:
-            result[g.seqname][g.attribute['gene_id']] = [g]
-        else:
-            result[g.seqname][g.attribute['gene_id']].append(g)
-    for chrom in result:
-        for gene in result[chrom]:
-            result[chrom][gene].sort(key=lambda x: x.start)
-    return result
 
 parser = argparse.ArgumentParser(description='')
 
 parser.add_argument('kleat', nargs='+', help='Kleat files')
 parser.add_argument('ensembl', help='GTF annotation file for ensembl. Must have "UTR" feature for every gene')
 parser.add_argument('aceview', help='GTF annotation file for aceview. Must have "UTR" feature for every gene')
+parser.add_argument('refseq', help='GTF annotation file for refseq')
+parser.add_argument('ucsc', help='GTF annotation file for ucsc')
 parser.add_argument('summary', help='Summary file for CCLE data')
 parser.add_argument('-x', '--min_novel_distance', type=int, default=20, help='The minimum distance between a cleavage site and an annotated transcript for the site to be deemed novel. Default = 20')
 parser.add_argument('-d', '--max_dist_ann', type=int, default=float('inf'), help='Maximum distance from annotated site. Default = infinity')
@@ -161,48 +86,108 @@ if not os.path.isdir(args.outdir):
 kleats = []
 annots = []
 
+pos = []
+neg = []
+
 # Parse KLEAT data
 N = len(args.kleat)
 for i,kleat in enumerate(args.kleat):
     sprint('Loading KLEAT {}/{} ...\r'.format(i+1,N))
-#    kleats += parseKleat(kleat,
-#                         args.max_dist_ann,
-#                         args.min_len_tail_contig,
-#                         args.min_num_tail_reads,
-#                         args.min_num_bridge_reads,
-#                         has_pas = args.has_polyadenylation_signal)
-    kleats += parseSimpleKleat(kleat,
-                         args.max_dist_ann,
-                         args.min_len_tail_contig,
-                         args.min_num_tail_reads,
-                         args.min_num_bridge_reads,
-                         has_pas = args.has_polyadenylation_signal)
+    kleats += Kleat.parseKleat(kleat,
+                               args.max_dist_ann,
+                               args.min_len_tail_contig,
+                               args.min_num_tail_reads,
+                               args.min_num_bridge_reads,
+                               args.min_bridge_read_tail_len,
+                               has_pas = args.has_polyadenylation_signal)
 print 'Loading KLEAT {}/{} ...DONE\r'.format(N,N)
 
 # Group KLEAT data
 sprint('Grouping kleat data ...')
-kleats = groupKleat(kleats)
+kleats = Kleat.groupKleat(kleats)
 print 'DONE'
 
 # Parse ensembl
 sprint('Parsing ensembl annotation ...')
-ensembl = parseGTF(args.ensembl, seqnames=set(['chr' + str(x) for x in range(1,24)] + ['X', 'Y']), sources=set(['protein_coding','retained_intron']), features=set(['transcript']))
+ensembl = parseGTF('/projects/dmacmillanprj2/polya/ccle/novel_cleavage_events/ensembl.Homo_sapiens.GRCh37.75.gtf', clean=True, seqnames=set(['chr' + str(x) for x in range(1,24)] + ['X', 'Y']), sources=set(['protein_coding','retained_intron']), features=set(['transcript']))
 print 'DONE'
 
 # Parse aceview
 sprint('Parsing aceview annotation ...')
-aceview = parseGTF(args.aceview, seqnames=set(['chr' + str(x) for x in range(1,24)] + ['X', 'Y']))
+aceview = parseGTF('/projects/dmacmillanprj2/polya/ccle/novel_cleavage_events/AceView.ncbi_37.genes_gff.gff', features=['exon'], seqnames=set(['chr' + str(x) for x in range(1,24)] + ['X', 'Y']))
 print 'DONE'
+
+# Parse refseq
+sprint('Parsing refseq annotation ...')
+refseq = parseGTF('/projects/dmacmillanprj2/polya/ccle/novel_cleavage_events/refseq.gtf.fixed.gz', gzipped=True, add_chr=False, features=['exon'])
+print 'DONE'
+
+# Parse ucsc
+sprint('Parsing ucsc annotation ...')
+ucsc = parseGTF('/projects/dmacmillanprj2/polya/ccle/novel_cleavage_events/ucsc.gtf.fixed.gz', gzipped=True, add_chr=False, features=['exon'])
+print 'DONE'
+
+pos = []
+neg = []
 
 # Group ensembl
 sprint('Grouping ensembl data ...')
-gensembl = groupEnsemblGTF(ensembl)
+gensembl = groupGTF(ensembl)
 print 'DONE'
+
+for c in gensembl:
+    for g in gensembl[c]:
+		for x in gensembl[c][g]:
+			strand = x.strand
+			if strand == '+':
+				pos.append(x)
+			else:
+				neg.append(x)
 
 # Group aceview
 sprint('Grouping aceview data ...')
-ggtf = groupAceviewGTF(aceview, gensembl)
+gaceview = groupGTF(aceview, group_by='gene_id', sort_stranded=True)
+groupBy(gaceview, 'transcript_id')
 print 'DONE'
+
+for c in gaceview:
+	for g in gaceview[c]:
+		for t in gaceview[c][g]:
+			strand = gaceview[c][g][t][0].strand
+			if strand == '+':
+				pos.append(gaceview[c][g][t][-1])
+			else:
+				neg.append(gaceview[c][g][t][0])
+
+# Group refseq
+sprint('Grouping refseq data ...')
+grefseq = groupGTF(refseq, group_by='gene_name', sort_stranded=True)
+groupBy(grefseq, 'transcript_id')
+print 'DONE'
+
+for c in grefseq:
+	for g in grefseq[c]:
+		for t in grefseq[c][g]:
+			strand = grefseq[c][g][t][0].strand
+			if strand == '+':
+				pos.append(grefseq[c][g][t][-1])
+			else:
+				neg.append(grefseq[c][g][t][0])
+
+# Group ucsc
+sprint('Grouping UCSC data ...')
+gucsc = groupGTF(ucsc, group_by='gene_name', sort_stranded=True)
+groupBy(gucsc, 'transcript_id')
+print 'DONE'
+
+for c in gucsc:
+	for g in gucsc[c]:
+		for t in gucsc[c][g]:
+			strand = gucsc[c][g][t][0].strand
+			if strand == '+':
+				pos.append(gucsc[c][g][t][-1])
+			else:
+				neg.append(gucsc[c][g][t][0])
 
 # Parse summary file
 sprint('Parsing summary file ...')
@@ -211,7 +196,7 @@ print 'DONE'
 
 # Find novel sites
 sprint('Finding novel sites ...')
-sites = findNovelCleavageEvents(kleats, ggtf, distance=args.min_novel_distance)
+sites = findNovelCleavageEvents(kleats, pos, neg, distance=args.min_novel_distance)
 print 'DONE'
 
 outfile = os.path.join(args.outdir, args.name)
